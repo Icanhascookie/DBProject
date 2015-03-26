269cf91d7fc2599f06ac1f00473c3a92da07196c
Johan Gran Nilsson <ada10jgr@falk-4.student.lth.se>
2015-03-26 13:54:18 +0100

    Connect stuff

--- database.inc.php
+++ database.inc.php
@@ -13,3 +13,52 @@ class Database {
 		$this->password = $password;
 		$this->database = $database;
 	}
+	
+	public function openConnection() {
+		try {
+			$this->conn = new PDO("mysql:host=$this->host;dbname=$this->database", 
+					$this->userName,  $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			$error = "Connection error: " . $e->getMessage();
+			print $error . "<p>";
+			unset($this->conn);
+			return false;
+		}
+		return true;
+	}
+
+	public function closeConnection() {
+		$this->conn = null;
+		unset($this->conn);
+	}
+
+	public function isConnected() {
+		return isset($this->conn);
+	}
+	
+	private function executeQuery($query, $param = null) {
+		try {
+			$stmt = $this->conn->prepare($query);
+			$stmt->execute($param);
+			$result = $stmt->fetchAll();
+		} catch (PDOException $e) {
+			$error = "*** Internal error: " . $e->getMessage() . "<p>" . $query;
+			die($error);
+		}
+		return $result;
+	}
+
+	private function executeUpdate($query, $param = null) {
+		try {
+			$stmt = $this->conn->prepare($query);
+			$stmt->execute($param);
+			return $stmt->rowCount();
+		} catch (PDOException $e) {
+			$error = "*** Internal error" . $e->getMessage() . "<p>" . $query;
+			$this->conn->rollBack();
+			die($error);
+		}
+	}
+	//Add Functions
+}?>
---
+++ mysql_connect_data.inc.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "puccini.cs.lth.se";
+	$userName = "db78";
+	$password = "wrs666uw";
+	$database = $userName;
+?>
