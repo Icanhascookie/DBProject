5c2e9dc1bf8b17b0810fe51a076a4f1a9236fb21
Icanhascookie <johan@makajopa.se>
2015-04-03 15:22:11 +0200

    Resending project

--- search.php
+++ search.php
@@ -13,16 +13,20 @@
 <html>
 <head><title>Search</title><head>
 <body><h1>Search</h1>
-	<form method=post action="searchDone.php">
-		Search for pallet by Pallet ID: <input type="text" name="searchValue">
-		<input type="hidden" name="type" value="id">
-		<input type=submit value="Search">
+	<form method = post action = "searchDone.php">
+		Search for pallet by Pallet ID: <input type = "text" name = "searchValue">
+		<input type = "hidden" name = "type" value = "id">
+		<input type = submit value = "Search">
 	</from>
 	------ <br>
 
+	<form method = post action = "searhDone-php">
+		Search for pallet by Cookie type and Time produced: <input type = "text" name = "searchyValue">
+		<input> type 
+
 <p>
-<form method=post action="index.php">
-	<input type=submit value="Return">
+<form method = post action = "index.php">
+	<input type = submit value = "Return">
 </form>
 
 </body>
